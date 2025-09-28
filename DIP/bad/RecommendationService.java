5d8af4cc80e75d1fd267c4c764c1257ae719ddca
Mahima Bharati Chirravuri <mahima@Mahimas-MacBook-Air.local>
2025-09-28 17:18:13 +0530

    Correcting bad example for DIP

--- DIP/bad/RecommendationService.java
+++ DIP/bad/RecommendationService.java
@@ -1,9 +1,5 @@
 public class RecommendationService {
     void showRecommendations() {
-        TrendingRecommendation trendingRecommendations = new TrendingRecommendation();
-        trendingRecommendations.recommend();
-        GenreRecommendation genreRecommendations = new GenreRecommendation();
-        genreRecommendations.recommend();
         RecentRecommendation recentRecommendations = new RecentRecommendation();
         recentRecommendations.recommend();
     }
